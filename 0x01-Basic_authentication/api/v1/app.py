fc4463e50b9babea3907d8fbb6e06cb7eaff40d6
parksomin72 <fatimazahraealawi@gmail.com>
2025-01-15 17:03:00 +0000

    Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -2,14 +2,21 @@
 
 from flask import Flask, jsonify
 from api.v1.views import app_views
-from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.errorhandler(401)
 def unauthorized_error(error):
+    """
+    Handles 401 Unauthorized errors.
+
+    Returns:
+        JSON response with the error message.
+    """
     return jsonify({"error": "Unauthorized"}), 401
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -2,8 +2,17 @@
 
 from flask import Blueprint, abort
 
+# Define the Blueprint for the app
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
 
 @app_views.route('/unauthorized', methods=['GET'])
 def unauthorized():
+    """
+    This function handles the /api/v1/unauthorized route.
+    It raises a 401 Unauthorized error using the abort function.
+
+    Returns:
+        JSON response with the error message.
+    """
     abort(401)
